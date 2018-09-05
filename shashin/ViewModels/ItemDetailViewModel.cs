7f8fbb566a896bdb87881a21f0eaa43e7755ab4e
chris <chrischong1997@gmail.com>
2018-09-05 13:17:12 +1000

    first commit

---
+++ shashin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using shashin.Models;
+
+namespace shashin.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
