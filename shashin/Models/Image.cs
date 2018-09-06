553c55a37506090e57ffd584adc8871bf1a03acc
chris <chrischong1997@gmail.com>
2018-09-06 21:04:33 +1000

    commit gallery update

---
+++ shashin/Models/Image.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace shashin.Models
+{
+    public class Image
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
