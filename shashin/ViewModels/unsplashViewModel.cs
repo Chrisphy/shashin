96f896fa63ac4b64aef420e9de12a6eebf7ff9e5
chris <chrischong1997@gmail.com>
2018-09-07 11:04:17 +1000

    commit API update

---
+++ shashin/ViewModels/unsplashViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Unsplasharp;
+
+namespace shashin.ViewModels
+{
+    public class unsplashViewModel
+    {
+        var client = new UnsplasharpClient("d20c6cd248561366ca2de82da5077eaee8d749433761e8d4605e4bcbc73a1c07");
+
+
+
+
+    }
+}
