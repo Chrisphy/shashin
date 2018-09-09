310b580415974efa2aaaf6051fbcf4d1bd2fe6e9
Steves161 <27788517+Steves161@users.noreply.github.com>
2018-09-09 14:46:11 +1000

    Database working for passcode!
    a DatabaseString class also added to be able to send string to database

---
+++ shashin/Models/DatabaseString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace shashin.Models
+{
+    public class DatabaseString
+    {
+        public string MyString { get; set; }
+        public DatabaseString()
+        {
+        }
+        public DatabaseString(string myString)
+        {
+            this.MyString = myString;
+        }
+    }
+}
--- shashin/Views/PasscodePage.xaml.cs
+++ shashin/Views/PasscodePage.xaml.cs
@@ -5,29 +5,52 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using MarcTron.Plugin.MTSql;
+using SQLite;
+using shashin.Models;
 
 namespace shashin.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class PasscodePage : ContentPage
 	{
+        public SQLiteAsyncConnection CF_Database { get; set; }
 		public PasscodePage ()
 		{
 			InitializeComponent ();
-            
 		}
         private void EntryPasscode_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
         }
-        private void EntryPasscode_Completed(object sender, EventArgs e)
+        private async void EntryPasscode_Completed(object sender, EventArgs e)
         {
-            if (EntryPasscode.Text == "1234")
+            CF_Database = MTSql.Current.GetConnectionAsync("passcode.db");
+            List<DatabaseString> listResult;
+            try
             {
-                App.Current.MainPage = new MainPage();
+                var list = await CF_Database.Table<DatabaseString>().ToListAsync();
+                listResult = list;
+            }
+            catch (SQLiteException)
+            {
+                listResult = null;
+            }
+            if (listResult == null)
+            {
+                CF_Database.CreateTableAsync<DatabaseString>().Wait();
+                await CF_Database.InsertAsync(new DatabaseString { MyString = EntryPasscode.Text } );
             }
             else
             {
-                EntryPasscode.Text = "Incorrect Passcode";
+                var list = await CF_Database.Table<DatabaseString>().ToListAsync();
+                if (EntryPasscode.Text == list[0].MyString)
+                {
+                    App.Current.MainPage = new MainPage();
+                }
+                else
+                {
+                    EntryPasscode.Text = "Incorrect Passcode";
+                }
             }
         }
         private void EntryPasscode_Focused(object sender, FocusEventArgs e)
