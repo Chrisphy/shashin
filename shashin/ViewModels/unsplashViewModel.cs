6d53feada927c9cb471b0fd0cb49c32e958d01b3
chris <chrischong1997@gmail.com>
2018-09-09 15:18:32 +1000

    Merging changes

--- shashin/ViewModels/unsplashViewModel.cs
+++ shashin/ViewModels/unsplashViewModel.cs
@@ -10,11 +10,6 @@ namespace shashin.ViewModels
 {
     public class unsplashViewModel : INotifyPropertyChanged
     {
-<<<<<<< HEAD
-        UnsplasharpClient client = new UnsplasharpClient("d20c6cd248561366ca2de82da5077eaee8d749433761e8d4605e4bcbc73a1c07");
-
-
-=======
         UnsplasharpClient client = new UnsplasharpClient("93123f0db401f8367e061a60e9b0976b9bc9c3cafe5133f344bba4010c97a4de",
                                                          "ec8401ec0727226a41f9fea4ef184c10f7efef4b009ee910dbf3ca386a");
         Photo _randomPhoto;
@@ -34,7 +29,6 @@ namespace shashin.ViewModels
         {
             FetchDataAsync();
         }
->>>>>>> 08e0ad1cba663f01374d0049b6c80c1eed6def1a
 
         public async Task FetchDataAsync()
         {
