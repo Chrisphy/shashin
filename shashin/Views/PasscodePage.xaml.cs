52d5813d75dd9ae4fad6534ae6ee87babf6b951d
Steves161 <27788517+Steves161@users.noreply.github.com>
2018-09-14 12:25:09 +1000

    Passcode new use
    -First time use of passcode asks to create a new passcode
    -Second and other times will not ask as passcode exists

--- shashin/Views/PasscodePage.xaml.cs
+++ shashin/Views/PasscodePage.xaml.cs
@@ -15,12 +15,35 @@ namespace shashin.Views
 	public partial class PasscodePage : ContentPage
 	{
         public SQLiteAsyncConnection CF_Database { get; set; }
-		public PasscodePage ()
-		{
-			InitializeComponent ();
-		}
+        public PasscodePage()
+        {
+            InitializeComponent();
+            CheckIfFirstTime();
+        }
         private void EntryPasscode_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
+        }
+        private async void CheckIfFirstTime()
+        {
+            CF_Database = MTSql.Current.GetConnectionAsync("passcode.db");
+            List<DatabaseString> listResult;
+            try
+            {
+                var list = await CF_Database.Table<DatabaseString>().ToListAsync();
+                listResult = list;
+            }
+            catch (SQLiteException)
+            {
+                listResult = null;
+            }
+            if (listResult == null)
+            {
+                FirstTimePasscode.IsVisible = true;
+            }
+            else
+            {
+                FirstTimePasscode.IsVisible = false;
+            }
         }
         private async void EntryPasscode_Completed(object sender, EventArgs e)
         {
@@ -39,6 +62,7 @@ namespace shashin.Views
             {
                 CF_Database.CreateTableAsync<DatabaseString>().Wait();
                 await CF_Database.InsertAsync(new DatabaseString { MyString = EntryPasscode.Text } );
+                App.Current.MainPage = new MainPage();
             }
             else
             {
