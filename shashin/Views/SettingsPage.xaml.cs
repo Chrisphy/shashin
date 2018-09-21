ec3387750772fd7a855d62d40d9d026a98ee284c
Steves161 <27788517+Steves161@users.noreply.github.com>
2018-09-21 18:51:41 +1000

    User Information Settings Page
    -Loads user information from database
    -Can Edit and Save details

--- shashin/Views/SettingsPage.xaml.cs
+++ shashin/Views/SettingsPage.xaml.cs
@@ -13,8 +13,33 @@ namespace shashin.Views
         public SettingsPage()
         {
             InitializeComponent();
+            GrabUserInformation();
         }
-
+        private async void GrabUserInformation()
+        {
+            CF_Database = MTSql.Current.GetConnectionAsync("userinfo.db");
+            List<DatabaseString> listResult;
+            try
+            {
+                var list = await CF_Database.Table<DatabaseString>().ToListAsync();
+                listResult = list;
+            }
+            catch (SQLiteException)
+            {
+                listResult = null;
+            }
+            if (listResult == null)
+            {
+                CF_Database.CreateTableAsync<DatabaseString>().Wait();
+                await CF_Database.InsertAsync(new DatabaseString { MyString = "Name" });
+                await CF_Database.InsertAsync(new DatabaseString { MyString = "LastName" });
+                await CF_Database.InsertAsync(new DatabaseString { MyString = "Not Specified" });
+            }
+            var Newlist = await CF_Database.Table<DatabaseString>().ToListAsync();
+            NewFirstNameTxt.Text = Newlist[0].MyString;
+            NewLastNameTxt.Text = Newlist[1].MyString;
+            NewGenderTxt.Text = Newlist[2].MyString;
+        }
         private async void SaveBtn_Clicked(object sender, EventArgs e)
         {
             if (NewPasscodeTxt.Text == null || NewPasscodeTxt.Text == "" || NewPasscodeTxt.Text == "Enter Passcode Here")
@@ -29,6 +54,15 @@ namespace shashin.Views
                 await CF_Database.InsertAsync(new DatabaseString { MyString = NewPasscodeTxt.Text });
                 NewPasscodeTxt.Text = "";
             }
+        }
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            CF_Database = MTSql.Current.GetConnectionAsync("userinfo.db");
+            CF_Database.DropTableAsync<DatabaseString>().Wait();
+            CF_Database.CreateTableAsync<DatabaseString>().Wait();
+            await CF_Database.InsertAsync(new DatabaseString { MyString = NewFirstNameTxt.Text });
+            await CF_Database.InsertAsync(new DatabaseString { MyString = NewLastNameTxt.Text });
+            await CF_Database.InsertAsync(new DatabaseString { MyString = NewGenderTxt.Text });
         }
     }
 }
