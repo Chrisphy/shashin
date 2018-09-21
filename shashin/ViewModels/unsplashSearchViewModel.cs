c22cdc98d826fb2d5524047101b27f98926cc775
HongDucNguyen <nduc@deakin.edu.au>
2018-09-21 17:39:48 +1000

    Search Implementation - Done

---
+++ shashin/ViewModels/unsplashSearchViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using shashin.Data;
+using Unsplasharp.Models;
+
+namespace shashin.ViewModels
+{
+    public class unsplashSearchViewModel: INotifyPropertyChanged
+    {
+        public static string searchString = null;
+
+        ObservableCollection<Photo> _listPhoto;
+
+        public ObservableCollection<Photo> ListPhoto
+        {
+            get { return _listPhoto; }
+            set
+            {
+                _listPhoto = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("ListPhoto"));
+                }
+            }
+        }
+
+
+        public async Task SearchDataAsync()
+        {
+            List<Photo> tmpList = null;
+            
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                tmpList = await PhotoManager.client.SearchPhotos(searchString, page: 1, perPage: 5);
+            }
+            ListPhoto = new ObservableCollection<Photo>(tmpList);
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    
+
+        public unsplashSearchViewModel()
+        {
+            SearchDataAsync();
+        }
+    }
+}
